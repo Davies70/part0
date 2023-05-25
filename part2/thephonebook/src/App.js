d5eb3f156d81e3e37e4996adae1bd510432ac578
Ajayi Davies <ajayidavies7@gmail.com>
2023-05-25 13:57:11 +0200

    Part 2 completed

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,105 +1,45 @@
 import { useState, useEffect } from 'react';
-import Filter from './Filter';
-import PersonForm from './PersonForm';
-import Persons from './Persons';
-import Notification from './Notification';
-import {
-  getPersons,
-  addPerson,
-  deletePerson,
-  updatePerson,
-} from './phoneServices';
+import axios from 'axios';
+import Countries from './Countries';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
-  const [message, setMessage] = useState(null);
-  const [error, setError] = useState(false);
+  const [countries, setCountries] = useState([]);
+  const [value, setValue] = useState('');
 
   useEffect(() => {
-    getPersons().then((data) => setPersons(data));
-  }, []);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newPersonObj = {
-      name: newName,
-      number: newNumber,
-    };
-
-    const foundPerson = persons.find((person) => person.name === newName);
-    if (typeof foundPerson !== 'undefined') {
-      if (
-        window.confirm(
-          `${foundPerson.name} is already added to phonebook, replace the old number with a new one?`
-        )
-      ) {
-        updatePerson(foundPerson.id, newPersonObj)
-          .then((data) =>
-            setPersons(
-              persons.map((person) =>
-                person.id !== foundPerson.id ? person : data
-              )
+    if (value) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then((response) => response.data)
+        .then((data) =>
+          setCountries(
+            data.filter((countryObj) =>
+              countryObj.name.common.toLowerCase().includes(value.toLowerCase())
             )
           )
-          .catch((error) => {
-            setError(true);
-            setMessage(
-              `Information of ${newName} has already been removed from server`
-            );
-            setTimeout(() => setMessage(null), 5000);
-          });
-        setMessage(`${newName}'s number changed`);
-        setTimeout(() => setMessage(null), 5000);
-      }
-    } else {
-      addPerson(newPersonObj).then((data) => setPersons(persons.concat(data)));
-      setMessage(`Added ${newName}`);
-      setTimeout(() => setMessage(null), 5000);
+        );
     }
-    setNewName('');
-    setNewNumber('');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
 
-  const handleNameInput = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberInput = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterInput = (event) => {
-    setFilter(event.target.value);
-    const filteredPersons = persons.filter((person) => {
-      return person.name.toLowerCase().includes(filter.toLowerCase());
-    });
-    setPersons(filteredPersons);
-  };
-
-  const handleDelete = (id, person) => {
-    if (window.confirm(`Delete ${person} ?`)) {
-      deletePerson(id);
-      setPersons(persons.filter((person) => person.id !== id));
-    }
-  };
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Notification message={message} error={error} />
-      <Filter filter={filter} handleFilterInput={handleFilterInput} />
-      <h3>Add a new</h3>
-      <PersonForm
-        handleSubmit={handleSubmit}
-        handleNameInput={handleNameInput}
-        handleNumberInput={handleNumberInput}
-        newName={newName}
-        newNumber={newNumber}
-      />
-      <h3>Numbers</h3>
-      <Persons persons={persons} handleDelete={handleDelete} />
+      <form>
+        find countries
+        <input type="search" onChange={handleChange} value={value}></input>
+      </form>
+
+      <div>
+        {countries.length > 10 ? (
+          <div>Too many matches, specifiy another filter</div>
+        ) : (
+          <Countries countries={countries} />
+        )}
+      </div>
     </div>
   );
 };
